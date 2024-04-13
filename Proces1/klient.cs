bbde9681bb40835836da0bd9167ebfe624626f60
makuszef <hu.makowski@gmail.com>
2024-04-13 15:59:58 -0400

    Dziala przesylanie obrazu

--- Proces1/klient.cs
+++ Proces1/klient.cs
@@ -17,33 +17,15 @@ class klient
 
         // Read the bytes from the file
         byte[] imageBytes = File.ReadAllBytes(filePath);
+        string base64String = Convert.ToBase64String(imageBytes);
         try
         {
-            // Create a TcpClient.
-            // Note, for this client to work you need to have a TcpServer
-            // connected to the same address as specified by the server, port
-            // combination.
-            Int32 port = 13000;
+            Int32 port = 13001;
             TcpClient client = new TcpClient("127.0.0.1", port);
-
-            // Translate the passed message into ASCII and store it as a Byte array.
-            String message = "Hello from client!";
-            Byte[] data = Encoding.ASCII.GetBytes(message);
-
-            // Get a client stream for reading and writing.
-            //  Stream stream = client.GetStream();
-
             NetworkStream stream = client.GetStream();
-
-            // Send the message to the connected TcpServer.
-            stream.Write(data, 0, data.Length);
-            Console.WriteLine("Sent: {0}", message);
-
-            // Receive the TcpServer.response.
-
-            // Buffer to store the response bytes.
-            data = new Byte[256];
-
+            stream.Write(imageBytes, 0, imageBytes.Length);
+            Console.WriteLine("Sent: {0}", base64String);
+/*
             // String to store the response ASCII representation.
             String responseData = String.Empty;
 
@@ -51,7 +33,7 @@ class klient
             Int32 bytes = stream.Read(data, 0, data.Length);
             responseData = Encoding.ASCII.GetString(data, 0, bytes);
             Console.WriteLine("Received: {0}", responseData);
-
+*/
             // Close everything.
             stream.Close();
             client.Close();
--- Proces3/serwer.py
+++ Proces3/serwer.py
@@ -1,6 +1,6 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-
+import base64
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -12,11 +12,16 @@ server.register_introspection_functions()
 
 # Define a function to be called remotely
 def add(x, y):
-    print("X:" + x)
     return x + y
-
+def handleImage(image):
+    image_data = base64.b64decode(image)
+    image_path = "decoded_image.jpg"
+    with open(image_path, "wb") as image_file:
+        image_file.write(image_data)
+    return "image"
 # Register the function with a different name
 server.register_function(add, 'add_numbers')
+server.register_function(handleImage, 'handleImage')
 
 # Run the server's main loop
 print("XML-RPC server is running on localhost:8000...")
